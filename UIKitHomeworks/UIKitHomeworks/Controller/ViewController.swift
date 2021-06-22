60b85d5426de286843282fabd1926ff71fdf13b0
Admalll <F1zzer1997195@gmail.com>
2021-06-22 16:41:39 +0300

    no message

--- UIKitHomeworks/UIKitHomeworks/Controller/ViewController.swift
+++ UIKitHomeworks/UIKitHomeworks/Controller/ViewController.swift
@@ -15,21 +15,26 @@ class ViewController: UIViewController {
     override func loadView() {
         super.loadView()
         self.view = messageView
+        messageView.messageButton.addTarget(self, action: #selector(tapStartButton), for: .touchUpInside)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        setupAlert()
+    func message(message: String) -> String {
+        return model.returnMessage(message: message)
     }
     
-    func setupAlert() {
+    private func setupAlert() {
         let alert = UIAlertController(title: "Enter message", message: nil, preferredStyle: .alert)
         alert.addTextField(configurationHandler: nil)
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (action) in
             guard let self = self else { return }
             guard let text = alert.textFields?.first?.text else { return }
-            self.messageView.messageLabel.text = self.model.returnMessage(message: text)
+            self.messageView.messageLabel.text = self.message(message: text)
         }))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func tapStartButton() {
+        setupAlert()
     }
 }
 
