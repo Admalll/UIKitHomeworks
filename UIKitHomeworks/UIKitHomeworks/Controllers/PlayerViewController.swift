8f23331f9061eb4f2700e7ed8e48ba9cbaee4dd6
Admalll <F1zzer1997195@gmail.com>
2021-06-25 11:31:55 +0300

    no message

---
+++ UIKitHomeworks/UIKitHomeworks/Controllers/PlayerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerViewController.swift
+//  UIKitHomeworks
+//
+//  Created by Vlad on 25.06.2021.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
