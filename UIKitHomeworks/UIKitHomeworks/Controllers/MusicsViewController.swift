666416d327881ab71b806f03f7f93117a2099fc4
Admalll <F1zzer1997195@gmail.com>
2021-06-25 12:53:15 +0300

    no message

--- UIKitHomeworks/UIKitHomeworks/Controllers/MusicsViewController.swift
+++ UIKitHomeworks/UIKitHomeworks/Controllers/MusicsViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class MusicsViewController: UIViewController {
 
+    @IBOutlet weak var firstSongLabel: UILabel!
+    @IBOutlet weak var secondSongLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
     }
-
-
+    
+    func setupView() {
+        let firstGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(firstSongTapped))
+        firstSongLabel.addGestureRecognizer(firstGestureRecognizer)
+        let secondGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(secondSongTapped))
+        secondSongLabel.addGestureRecognizer(secondGestureRecognizer)
+    }
+    
+    @objc func firstSongTapped() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier: "player")
+        self.show(secondVC, sender: self)
+    }
+    
+    @objc func secondSongTapped() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier: "player")
+        self.show(secondVC, sender: self)
+    }
 }
 
