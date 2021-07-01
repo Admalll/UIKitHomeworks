fce021b323fe55ff5be2885b675153380787f708
Admalll <F1zzer1997195@gmail.com>
2021-07-01 22:04:39 +0300

    no message

--- UIKitHomeworks/UIKitHomeworks/AlarmViewController.swift
+++ UIKitHomeworks/UIKitHomeworks/AlarmViewController.swift
@@ -18,28 +18,33 @@ class AlarmViewController: UIViewController {
     }
 }
 
-extension CALayer {
-var borderWidthIB: NSNumber {
-    get {
-        return NSNumber(value: Float(borderWidth))
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
     }
-    set {
-        borderWidth = CGFloat(newValue.floatValue)
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
     }
-}
-var borderColorIB: UIColor? {
-    get {
-        return borderColor != nil ? UIColor(cgColor: borderColor!) : nil
-    }
-    set {
-        borderColor = newValue?.cgColor
-    }
-}
-var cornerRadiusIB: NSNumber {
-    get {
-        return NSNumber(value: Float(cornerRadius))
-    }
-    set {
-        cornerRadius = CGFloat(newValue.floatValue)
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
     }
 }
--- UIKitHomeworks/UIKitHomeworks/TimerViewController.swift
+++ UIKitHomeworks/UIKitHomeworks/TimerViewController.swift
@@ -8,20 +8,61 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var hour: Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
     }
-    */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 25
+        case 1,2:
+            return 60
 
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/3
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) ч"
+        case 1:
+            return "\(row) мин"
+        case 2:
+            return "\(row) с"
+        default:
+            return ""
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            hour = row
+        case 1:
+            minutes = row
+        case 2:
+            seconds = row
+        default:
+            break
+        }
+    }
 }
