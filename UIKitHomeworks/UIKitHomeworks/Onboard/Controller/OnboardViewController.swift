2f7e78988720c6d874c806ce92f468584373a37b
Admalll <F1zzer1997195@gmail.com>
2021-07-06 20:00:56 +0300

    no message

--- UIKitHomeworks/UIKitHomeworks/Onboard/Controller/OnboardViewController.swift
+++ UIKitHomeworks/UIKitHomeworks/Onboard/Controller/OnboardViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class OnboardViewController: UIViewController {
+final class OnboardViewController: UIViewController {
+
+    //MARK: - Private properties
+    private let defaults = UserDefaults.standard
+    private var isLogged: Bool {
+        get {
+            return defaults.bool(forKey: "LOGGED_IN_KEY")
+        } set {
+            defaults.setValue(newValue, forKey: "LOGGED_IN_KEY")
+        }
+    }
 
     //MARK: - UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        checkIsLoged()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -20,6 +30,16 @@ class OnboardViewController: UIViewController {
         navigationController?.navigationBar.shadowImage = UIImage()
     }
 
+    //MARK: - Private methods
 
+    private func checkIsLoged() {
+        if isLogged {
+            let sb = UIStoryboard(name: "Friends", bundle: nil)
+            let vc = sb.instantiateViewController(identifier: "FriendsTabBarController")
+            navigationController?.pushViewController(vc, animated: true)
+        } else {
+            isLogged = true
+        }
+    }
 }
 
