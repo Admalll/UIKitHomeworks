0c932c9d6e6bce66c52c7e2fa39494632a6ade94
Admalll <F1zzer1997195@gmail.com>
2021-08-01 10:04:50 +0300

    Реализовал светофор кодом, на двух экранах, с помощью: - якорей - констрейнтов

---
+++ UIKitHomeworks/UIKitHomeworks/Application/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  UIKitHomeworks
+//
+//  Created by Vlad on 21.06.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
